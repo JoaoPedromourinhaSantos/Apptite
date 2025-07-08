e6c187ab27163f5cb82f4e1ddf4b6cd53ea75a99
João Moura <133607838+JoaoPedromourinhaSantos@users.noreply.github.com>
2025-07-08 13:21:42 -0300

    Add files via upload

---
+++ src/test/java/com/apptite/apptite/ApptiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apptite.apptite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApptiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
